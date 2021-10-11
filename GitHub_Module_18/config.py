6be421702dea2eb086ea89c2217ce73d9ec7ebee
AndreyKizimenko <87822796+AndreyKizimenko@users.noreply.github.com>
2021-10-11 15:38:25 -0500

    Add files via upload

---
+++ GitHub_Module_18/config.py
@@ -0,0 +1,10 @@
+TOKEN = "2090098547:AAFm3mMjWOsCNsXhB8c3F5KZbKCPk0VtUQg"
+
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'гривна': 'UAH',
+    'тенге': 'KZT',
+    'фунт': 'GBP'
+}
